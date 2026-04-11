345e2b58f6c6a2745e99b2e3f72f30284fe1a500
Paweł Torba <79547366+PawelTorba@users.noreply.github.com>
2026-04-11 19:17:26 +0200

    Changed data formating + minor fixes

---
+++ src/src/include/water.h
@@ -0,0 +1,14 @@
+#ifndef WATER_H
+#define WATER_H
+
+// FLOOD PINOUT
+#define WATER_OUT_PIN 25
+#define WATER_IN_PIN  26
+
+void triggerAlarm_Water(int now);
+void handleWaterSensor(bool simulationMode, int now);
+
+// SIMULATION
+
+
+#endif // WATER_H
---
+++ src/src/sensors/water.cpp
@@ -0,0 +1,25 @@
+#include "water.h"
+#include <Arduino.h>
+#include <stdlib.h>
+#include <time.h>
+
+// ===== ALERTS =====
+void triggerAlarm_Water(int now) {
+    //TO DO: ADD LED ALERT
+    //TO DO: SEND payload TO AZURE IOT HUB
+}
+
+
+
+void handleWaterSensor(bool simulationMode, int now) {
+    //jeżeli stan wody jest niski (0) to wysyła alert o zalaniu
+    digitalWrite(WATER_OUT_PIN, HIGH);
+    int waterState = digitalRead(WATER_IN_PIN);
+ 
+    char payload[256];
+    snprintf(payload, sizeof(payload),
+    "{\"sensorType\":\"water\",\"timestamp\":%d,\"alert\":%s}",
+    now, (waterState == 0) ? "true" : "false");
+    
+    Serial.printf("[WATER] Payload: %s\r\n", payload);
+}
